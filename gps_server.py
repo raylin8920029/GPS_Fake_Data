341791fdabb01800cd242fcea78d96d0eb63158e
Ray <ray.lin@vivotek.com>
2017-08-15 13:21:28 +0800

    Fix issues

--- gps_server.py
+++ gps_server.py
@@ -24,16 +24,16 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
             while True:
                 self.send_message(lines, g_args.interval)
                 self.line_index += 1
-                if self.line_index > self.end_of_line:
+                if self.line_index > self.end_of_line :
                     self.line_index = 0
         else:
             while repeat:
                 self.send_message(lines, g_args.interval)
                 self.line_index += 1
-                if self.line_index > self.end_of_line:
+                if self.line_index > self.end_of_line :
                     self.line_index = 0
                 repeat = repeat - 1
-
+        
     def parse_file(self, lines):
         gprmc_time = datetime.utcnow().strftime("%H%M%S")
         gprmc_date = datetime.utcnow().strftime("%d%m%y")
@@ -43,21 +43,15 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         latitude_hemisphere = "S" if coordinate_latitude < 0 else "N"
         longitude_hemisphere = "W" if coordinate_longitude < 0 else "E"
         gprmc_latitude = self.dd_to_gprmc_dms_format(coordinate_latitude)
-        if coordinate_latitude < 0:
-            gprmc_latitude *= -1
         gprmc_longitude = self.dd_to_gprmc_dms_format(coordinate_longitude)
-        if coordinate_longitude < 0:
-            gprmc_longitude *= -1
 
-        gprmc = gps_data_template.get_gps_data("GPRMC", Time=gprmc_time, Latitude=gprmc_latitude,
-                                           Latitude_Hemisphere=latitude_hemisphere, Longitude=gprmc_longitude,
-                                           Longitude_Hemisphere=longitude_hemisphere, Date=gprmc_date)
+        gprmc = gps_data_template.get_gps_data("GPRMC", Time=gprmc_time, Latitude=gprmc_latitude, Latitude_Hemisphere=latitude_hemisphere, Longitude=gprmc_longitude, Longitude_Hemisphere=longitude_hemisphere, Date=gprmc_date)
 
         msgList = []
-        if gprmc.find('\r\n') < 0:
-            msgList.append(gprmc + '\r\n')
+        if gprmc.find('\r\n') < 0 :
+            msgList.append(gprmc + '\r\n');
         return msgList
-
+                 
     def send_message_to_client(self, message):
         print 'sending "%s"' % message
         self.request.sendall(message)
@@ -74,29 +68,25 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         degs, d_int = math.modf(degs)
         mins, m_int = math.modf(60 * degs)
         secs = 60 * mins
-        gprmc_dms_format = d_int * 100 + m_int + (secs / 100)
+        gprmc_dms_format = d_int*100 + m_int + (secs/100)
         return gprmc_dms_format
-
 
 def main():
     parser = argparse.ArgumentParser(description='Send message to client')
     parser.add_argument('srv_port', help='port of server')
     parser.add_argument('file_name', help='File name of the message')
-    parser.add_argument('-r', "--repeat", default=-1, type=int,
-                        help='Specify how many times you want to send the message, -1 means infinite')
-    parser.add_argument('-i', "--interval", default=3, type=int,
-                        help='Sets the interval between sending message, in seconds')
-
+    parser.add_argument('-r', "--repeat", default=-1, type=int, help='Specify how many times you want to send the message, -1 means infinite')
+    parser.add_argument('-i', "--interval", default=3, type=int, help='Set the interval for sending message, in seconds')
+    
     global g_args
     g_args = parser.parse_args()
     try:
-        # Create the server, binding to localhost
+        # Create gps server, binding to localhost
         server = SocketServer.TCPServer(('0.0.0.0', int(g_args.srv_port)), MyTCPHandler)
         server.serve_forever()
     except KeyboardInterrupt:
         server.shutdown()
         server.server_close()
-
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     options = {'py2exe': {
         'bundle_files': 1
     }},
-    console = [{'script': 'gps_server.py',"icon_resources": [(1, "icon.png")]}],
+    console = [{'script': 'gps_server.py'}],
 	zipfile = None
 )
